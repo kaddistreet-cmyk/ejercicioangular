76a9ad739b7526cc79d09e00e51b9723107365df
kaddistreet-cmyk <kaddistreet@gmail.com>
2026-08-07 23:38:32 -0500

    feat: implementar formulario de contacto

--- src/app/contacto/contacto.ts
+++ src/app/contacto/contacto.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contacto',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './contacto.html',
   styleUrl: './contacto.css',
 })
-export class Contacto {}
+export class Contacto {
+  private formBuilder = inject(FormBuilder);
+
+  formularioContacto = this.formBuilder.group({
+    name: ['', [Validators.required, Validators.maxLength(20)]],
+    email: ['', [Validators.required, Validators.email]],
+    telefono: ['', [Validators.required, Validators.maxLength(10)]],
+  });
+
+  enviar() {
+    if (this.formularioContacto.valid) {
+      alert('Gracias por contactar a Nomadaware');
+    } else {
+      alert('Los datos ingresados son incorrectos');
+    }
+  }
+}
