f898060f43edce5c56ba2663334b6e80cecbd2dd
kaddistreet-cmyk <kaddistreet@gmail.com>
2026-08-07 23:40:10 -0500

    feat: implementar contenido dinámico del inicio

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -6,4 +6,29 @@ import { Component } from '@angular/core';
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home {
+  imagenInicio = '/images/nomadaware-home.webp';
+
+  servicios = [
+    {
+      id: 1,
+      nombre: 'Componentes',
+      descripcion: 'Memorias, discos y otros productos.',
+      imagen:
+        'https://nomadaware.com.ec/wp-content/uploads/NomadaWare_tarjeta_de_video_asus_rog_nvidia_rtx_4090_24gb_oc.png',
+    },
+    {
+      id: 2,
+      nombre: 'Equipos',
+      descripcion: 'Opciones para el hogar, estudio y trabajo.',
+      imagen:
+        'https://nomadaware.com.ec/wp-content/uploads/NomadaWare_audifono_hyperx_cloud_core_dts-2.png',
+    },
+    {
+      id: 3,
+      nombre: 'Atención',
+      descripcion: 'Información sencilla para cada compra.',
+      imagen: '',
+    },
+  ];
+}
