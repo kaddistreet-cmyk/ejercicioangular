d7fc63e03c75fc2516c4e5325be3161cddd88498
kaddistreet-cmyk <kaddistreet@gmail.com>
2026-08-07 23:40:45 -0500

    feat: implementar contenido dinámico de nosotros

--- src/app/nosotros/nosotros.ts
+++ src/app/nosotros/nosotros.ts
@@ -6,4 +6,25 @@ import { Component } from '@angular/core';
   templateUrl: './nosotros.html',
   styleUrl: './nosotros.css',
 })
-export class Nosotros {}
+export class Nosotros {
+  informacion = [
+    {
+      id: 1,
+      titulo: 'Misión',
+      descripcion: 'Ofrecer productos útiles y buena atención.',
+      imagen: 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/icons/bullseye.svg',
+    },
+    {
+      id: 2,
+      titulo: 'Visión',
+      descripcion: 'Ser una empresa confiable en el área tecnológica.',
+      imagen: 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/icons/eye.svg',
+    },
+    {
+      id: 3,
+      titulo: 'Valores',
+      descripcion: 'Responsabilidad, respeto y honestidad.',
+      imagen: '',
+    },
+  ];
+}
